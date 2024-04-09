d78c0e042344f2979136da862e36c12b57ff42d1
Laahini <62526180+Laahini@users.noreply.github.com>
2024-04-08 21:20:51 -0400

    update major

--- scripts/jobs.js
+++ scripts/jobs.js
@@ -2,49 +2,50 @@ var data = [
 	{
         jobName: "Community Outreach Coordinator",
         jobLocation: "Remote",
-        careerType: "Policy",
+        careerType: "Policy & Advocacy",
         jobType: "Full-Time-Contractor",
         descrip: "The candidate should have excellent communication skills and a fervor for encouraging community participation in environmental initiatives. Assuming the role of Community Outreach Coordinator means playing a vital part in The Green Place's mission by engaging with communities, promoting a shared sense of responsibility, and encouraging positive change toward a more sustainable future.",
         jobAppDescrip: "jobs/communityOutreachCoordinator.html"
 	},
 	{
-        jobName: "Community Outreach Coordinator",
-        jobLocation: "Remote",
-        careerType: "Policy",
+        jobName: "Sustainability Program Manager",
+        jobLocation: "New York City, NY",
+        careerType: "Project Management",
         jobType: "Full-Time-Contractor",
-        descrip: "The candidate should have excellent communication skills and a fervor for encouraging community participation in environmental initiatives. Assuming the role of Community Outreach Coordinator means playing a vital part in The Green Place's mission by engaging with communities, promoting a shared sense of responsibility, and encouraging positive change toward a more sustainable future.",
-        jobAppDescrip: "jobs/communityOutreachCoordinator.html"
+        descrip: "We are searching for a capable Sustainability Program Manager to oversee and implement our environmental initiatives. This involves crafting and executing diverse sustainability strategies, evaluating and reducing The Green Place's environmental footprint, and ensuring compliance with regulations.",
+        jobAppDescrip: "jobs/sustainabilityProgramManager.html"
 	},
 	{
-        jobName: "Community Outreach Coordinator",
-        jobLocation: "Remote",
-        careerType: "Policy",
-        jobType: "Part-Time-Contractor",
-        descrip: "The candidate should have excellent communication skills and a fervor for encouraging community participation in environmental initiatives. Assuming the role of Community Outreach Coordinator means playing a vital part in The Green Place's mission by engaging with communities, promoting a shared sense of responsibility, and encouraging positive change toward a more sustainable future.",
-        jobAppDescrip: "jobs/communityOutreachCoordinator.html"
+        jobName: "Environmental Education Specialist",
+        jobLocation: "New York City, NY",
+        careerType: "Policy & Advocacy",
+        jobType: "Internship",
+        descrip: "We are looking for an enthusiastic individual to take on the role of Environmental Education Specialist. This position entails creating and executing educational programs aimed at raising awareness about environmental issues and advocating for sustainable practices.",
+        jobAppDescrip: "jobs/environmentalEducationSpecialist.html"
 	},
 	{
-        jobName: "Community Outreach Coordinator",
-        jobLocation: "Remote",
-        careerType: "Advocacy",
+        jobName: "Sustainable Systems Analyst",
+        jobLocation: "Napa, CA",
+        careerType: "Research and Development",
         jobType: "Full-Time-Contractor",
         descrip: "The candidate should have excellent communication skills and a fervor for encouraging community participation in environmental initiatives. Assuming the role of Community Outreach Coordinator means playing a vital part in The Green Place's mission by engaging with communities, promoting a shared sense of responsibility, and encouraging positive change toward a more sustainable future.",
-        jobAppDescrip: "jobs/communityOutreachCoordinator.html"
+        jobAppDescrip: "jobs/sustainableSystemsAnalyst.html"
 	},
 	{
-        jobName: "Community Outreach Coordinator",
+        jobName: "Sr. Data Analytics Engineer",
         jobLocation: "Remote",
-        careerType: "Advocacy",
-        jobType: "Full-Time-Contractor",
-        descrip: "The candidate should have excellent communication skills and a fervor for encouraging community participation in environmental initiatives. Assuming the role of Community Outreach Coordinator means playing a vital part in The Green Place's mission by engaging with communities, promoting a shared sense of responsibility, and encouraging positive change toward a more sustainable future.",
-        jobAppDescrip: "jobs/communityOutreachCoordinator.html"
+        careerType: "Engineering",
+        jobType: "Permanent Full-Time",
+        descrip: "We are seeking a hands-on, self-motivated Data Analytics Engineer to own our data analytics pipeline for one of our fastest growing businesses. In this role, you will be responsible for collecting and modeling data through the deployment of modern cloud tools, and turning that data into insights through the building of dashboards and reports. ",
+        jobAppDescrip: "jobs/dataAnalyticsEngineer.html"
 	},
 	{
+        jobName: "Sr. Software Engineer",
         jobLocation: "Remote",
-        careerType: "Policy",
-        jobType: "Full-Time-Contractor",
-        descrip: "The candidate should have excellent communication skills and a fervor for encouraging community participation in environmental initiatives. Assuming the role of Community Outreach Coordinator means playing a vital part in The Green Place's mission by engaging with communities, promoting a shared sense of responsibility, and encouraging positive change toward a more sustainable future.",
-        jobAppDescrip: "jobs/communityOutreachCoordinator.html"
+        careerType: "Engineering",
+        jobType: "Permanent Full-Time",
+        descrip: "We are looking for a team member to help implement new data pipelines, expand our growing data infrastructure, and lead the development of new on-site capabilities across multiple domains.  If you live and breathe web development, love coming up with creative technical solutions, and are excited to join a small agile team, this role is a perfect opportunity for you.",
+        jobAppDescrip: "jobs/softwareEngineer.html"
 	}
 ];
 
@@ -166,3 +167,64 @@ $("#search-form").submit(function (e) {
 		}
 	});
 });
+
+
+
+
+
+
+
+
+
+const dropContainer = document.getElementById("dropcontainer");
+const fileInput = document.getElementById("resume");
+
+dropContainer.addEventListener("dragover", e => {
+  e.preventDefault();
+}, false);
+
+dropContainer.addEventListener("dragenter", () => {
+  dropContainer.classList.add("drag-active");
+});
+
+dropContainer.addEventListener("dragleave", () => {
+  dropContainer.classList.remove("drag-active");
+});
+
+dropContainer.addEventListener("drop", e => {
+  e.preventDefault();
+  dropContainer.classList.remove("drag-active");
+  fileInput.files = e.dataTransfer.files;
+});
+
+
+
+const dropContainer2 = document.getElementById("dropcontainer2");
+const fileInput2 = document.getElementById("cletter");
+
+dropContainer2.addEventListener("dragover", e => {
+  e.preventDefault();
+}, false);
+
+dropContainer2.addEventListener("dragenter", () => {
+  dropContainer2.classList.add("drag-active");
+});
+
+dropContainer2.addEventListener("dragleave", () => {
+  dropContainer2.classList.remove("drag-active");
+});
+
+dropContainer2.addEventListener("drop", e => {
+  e.preventDefault();
+  dropContainer2.classList.remove("drag-active");
+  fileInput2.files = e.dataTransfer.files;
+});
+
+
+function submitJob(){
+  location.href="congrats.html";
+}
+
+function openCareers(){
+    location.href="../careers.html";
+  }
